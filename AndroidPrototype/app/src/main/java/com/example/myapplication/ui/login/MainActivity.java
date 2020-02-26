d34b65cd5feee3f6cffc50488e2a9faaeb3f6544
Muhammed Hassan Mahmood <43229504+Heskpar@users.noreply.github.com>
2020-02-26 15:24:50 -0800

    initial commit
    Transferring all of my files from my existing project.

---
+++ AndroidPrototype/app/src/main/java/com/example/myapplication/ui/login/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ AndroidPrototype/app/src/main/java/com/example/myapplication/ui/login/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.ui.login;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.Button;
+import android.view.View;
+//import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+            }
+        });
+
+
+    }
+
+}
