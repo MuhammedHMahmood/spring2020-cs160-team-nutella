d9b35e9fb5ba482fb09399f27c01d29692c6745c
Heskpar <mohammedmahmood823@gmail.com>
2020-03-30 14:37:08 -0700

    Made new app from scratch
    new app, has log-ing and register pages

---
+++ Prototype1/app/src/main/java/com/example/prototype1/ui/login/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.prototype1.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prototype1.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
