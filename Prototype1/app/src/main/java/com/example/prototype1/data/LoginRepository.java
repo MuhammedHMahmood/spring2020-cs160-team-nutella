e5c2acdda51fa9479160d734420b148ef420a047
micompany4 <michael.j.wong@sjsu.edu>
2020-04-02 17:22:03 -0700

    "completed" login and register
    I am once again asking for your technological support

--- Prototype1/app/src/main/java/com/example/prototype1/data/LoginRepository.java
+++ Prototype1/app/src/main/java/com/example/prototype1/data/LoginRepository.java
@@ -1,7 +1,7 @@
 package com.example.prototype1.data;
 
 import com.example.prototype1.data.model.LoggedInUser;
-
+import com.example.prototype1.data.ResigterDataSource;
 /**
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
@@ -11,6 +11,7 @@ public class LoginRepository {
     private static volatile LoginRepository instance;
 
     private LoginDataSource dataSource;
+    private ResigterDataSource dataSource2;
 
     // If user credentials will be cached in local storage, it is recommended it be encrypted
     // @see https://developer.android.com/training/articles/keystore
@@ -46,6 +47,15 @@ public class LoginRepository {
     public Result<LoggedInUser> login(String username, String password) {
         // handle login
         Result<LoggedInUser> result = dataSource.login(username, password);
+        if (result instanceof Result.Success) {
+            setLoggedInUser(((Result.Success<LoggedInUser>) result).getData());
+        }
+        return result;
+    }
+
+    public Result<LoggedInUser> register(String username, String password) {
+        // handle register
+        Result<LoggedInUser> result = dataSource2.register(username, password);
         if (result instanceof Result.Success) {
             setLoggedInUser(((Result.Success<LoggedInUser>) result).getData());
         }
