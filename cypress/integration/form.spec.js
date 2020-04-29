8437802ce4416b39efb48bf9fae301c1fac19de2
micompany4 <michael.j.wong@sjsu.edu>
2020-04-28 21:03:37 -0700

    automate test for resgister
    able to run cypress to automate tests regarding the register page

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -1,7 +1,13 @@
+const text = "qwerty7";
+
 describe("Form test", () => {
   it("Can fill the form", () => {
     cy.visit("/");
     cy.get("form");
+
+    cy.get('input[name="name"]')
+      .type("michael")
+      .should("have.value", "michael");
 
     cy.get('input[name="email"]')
       .type("michael@gmail.com")
@@ -10,6 +16,14 @@ describe("Form test", () => {
     cy.get('input[name="psw"]')
       .type("qwerty7")
       .should("have.value", "qwerty7");
+
+      cy.get('input[name="psw-repeat"]')
+      .type("qwerty7")
+      .should("have.value", text);
+
+      cy.get('input[name="address"]')
+      .type("1234 street street")
+      .should("have.value", "1234 street street");
 
       cy.get("form").submit();
     
